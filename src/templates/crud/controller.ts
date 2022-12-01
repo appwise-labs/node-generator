9cb9f497f0c3d532c5f792fe905a2d99eb1fdc3d
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2022-12-01 14:27:05 +0100

    update transformer

--- src/templates/crud/controller.ts
+++ src/templates/crud/controller.ts
@@ -2,7 +2,7 @@ export const controller = (kebabCase: string, pascalCase: string, camelCase: str
   return `import { Request } from 'express'
 import { Create${pascalCase}DTO } from '../dtos/create-${kebabCase}.dto'
 import { ${pascalCase}DefaultService, ${pascalCase}Service } from '../services/${kebabCase}.service'
-import { ${pascalCase}Transformer, ${pascalCase}TransormerType } from '../transformers/${kebabCase}.transformer'
+import { ${camelCase}Transformer, ${pascalCase}TransformerType } from '../transformers/${kebabCase}.transformer'
 
 export class ${pascalCase}Controller {
   private readonly ${camelCase}Service: ${pascalCase}Service
@@ -17,28 +17,28 @@ export class ${pascalCase}Controller {
     this.delete${pascalCase} = this.delete${pascalCase}.bind(this)
   }
 
-  async get${pascalCase}s (_req: Request): Promise<${pascalCase}TransormerType[]> {
+  async get${pascalCase}s (_req: Request): Promise<${pascalCase}TransformerType[]> {
     const ${camelCase}s = await this.${camelCase}Service.get${pascalCase}s()
 
-    return ${pascalCase}Transformer.array(${camelCase}s)
+    return ${camelCase}Transformer.array(${camelCase}s)
   }
 
-  async get${pascalCase} (req: Request): Promise<${pascalCase}TransormerType> {
+  async get${pascalCase} (req: Request): Promise<${pascalCase}TransformerType> {
     const ${camelCase} = await this.${camelCase}Service.get${pascalCase}(req.params.${camelCase})
 
-    return ${pascalCase}Transformer.item(${camelCase})
+    return ${camelCase}Transformer.item(${camelCase})
   }
 
-  async create${pascalCase} (req: Request, dto: Create${pascalCase}DTO): Promise<${pascalCase}TransormerType> {
+  async create${pascalCase} (req: Request, dto: Create${pascalCase}DTO): Promise<${pascalCase}TransformerType> {
     const ${camelCase} = await this.${camelCase}Service.create${pascalCase}(dto)
 
-    return ${pascalCase}Transformer.item(${camelCase})
+    return ${camelCase}Transformer.item(${camelCase})
   }
 
-  async update${pascalCase} (req: Request, dto: Create${pascalCase}DTO): Promise<${pascalCase}TransormerType> {
+  async update${pascalCase} (req: Request, dto: Create${pascalCase}DTO): Promise<${pascalCase}TransformerType> {
     const ${camelCase} = await this.${camelCase}Service.update${pascalCase}(req.params.${camelCase}, dto)
 
-    return ${pascalCase}Transformer.item(${camelCase})
+    return ${camelCase}Transformer.item(${camelCase})
   }
 
   async delete${pascalCase} (req: Request): Promise<void> {
--- src/templates/crud/transformer.ts
+++ src/templates/crud/transformer.ts
@@ -1,19 +1,16 @@
 export const transformer = (kebabCase: string, pascalCase: string, camelCase: string): string => {
-  return `import { ${pascalCase} } from '../models/${kebabCase}.model'
+  return `import { Transformer } from '@appwise/express-dto-router'
+import { ${pascalCase} } from '../models/${kebabCase}.model'
 
-export interface ${pascalCase}TransormerType {
+export interface ${pascalCase}TransformerType {
   uuid: string
   createdAt: string
   updatedAt: string
   deletedAt: string | null
 }
 
-export class ${pascalCase}Transformer {
-  static item (${camelCase}: ${pascalCase}): ${pascalCase}TransormerType
-  static item (${camelCase}?: ${pascalCase} | null): ${pascalCase}TransormerType | null | undefined
-  static item (${camelCase}?: ${pascalCase} | null): ${pascalCase}TransormerType | null | undefined {
-    if (${camelCase} == null) return ${camelCase}
-
+class ${pascalCase}Transformer extends Transformer<${pascalCase}, ${pascalCase}TransformerType> {
+  transform (${camelCase}: ${pascalCase}): ${pascalCase}TransformerType {
     return {
       uuid: ${camelCase}.uuid,
       createdAt: ${camelCase}.createdAt.toISOString(),
@@ -21,12 +18,8 @@ export class ${pascalCase}Transformer {
       deletedAt: ${camelCase}.deletedAt?.toISOString() ?? null
     }
   }
-
-  static array (${camelCase}s: ${pascalCase}[]): ${pascalCase}TransormerType[] {
-    if (${camelCase}s == null) return ${camelCase}s
-
-    return ${camelCase}s.map(${camelCase} => ${pascalCase}Transformer.item(${camelCase}))
-  }
 }
+
+export const ${camelCase}Transformer = new ${pascalCase}Transformer()
 `
 }
